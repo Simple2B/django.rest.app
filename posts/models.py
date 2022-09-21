9d7e71d8a46b9b50526b7d93ae3f8b1fdb87f57f
EvgeniiShcherbina <eugene@inkit.com>
2022-09-21 14:50:23 +0300

    post model change

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     content = models.CharField(max_length=512, null=False)
     published = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    user_profile = models.ForeignKey('profiles.UserProfile', on_delete=models.PROTECT)
+    user_profile = models.ForeignKey('profiles.UserProfile', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"<{self.id}: {self.title} at {self.created_at}>"
