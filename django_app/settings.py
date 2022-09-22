75f357b805aa9b97e81dc88da73cf9fa6ecd0642
EvgeniiShcherbina <eugene@inkit.com>
2022-09-22 11:25:53 +0300

    add sample.env

--- django_app/settings.py
+++ django_app/settings.py
@@ -122,14 +122,14 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if os.environ['DJANGO_ENV'] == 'development':
+if os.environ.get('DJANGO_ENV') == 'development':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-elif os.environ['DJANGO_ENV'] == 'production':
+elif os.environ.get('DJANGO_ENV') == 'production':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -140,17 +140,6 @@ elif os.environ['DJANGO_ENV'] == 'production':
             'PORT': int(os.environ.get('LOCAL_DB_PORT')),
         }
     }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': 'db',
-#         'PORT': int(os.environ.get('LOCAL_DB_PORT')),
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
